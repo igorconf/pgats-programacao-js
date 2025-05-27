854c6a98eddf842681772e00d5f02cef7f64abc6
IgorCruz <igorconf@hotmail.com>
2025-05-27 20:18:12 -0300

    novo operador

---
+++ conceitos/operadores-aritmeticos.js
@@ -0,0 +1,15 @@
+/*
+ + adicao
+ - subtracao
+ * multiplicador
+ / divisao
+ % resto
+ ** exponencial
+*/
+
+console.log(1 + 1)
+console.log(10 - 7)
+console.log(2 * 2)
+console.log(11 / 2)
+console.log(11 % 2)
+console.log(2 ** 5)
