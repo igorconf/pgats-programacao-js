ec5c315dff789e8efd60ed1d43ad9d6bb883fff1
IgorCruz <igorconf@hotmail.com>
2025-05-28 17:52:23 -0300

    new folder

---
+++ exercicios/03-exercicio.js
@@ -0,0 +1,12 @@
+const nome = "Ada"
+const idade = 1
+const porte = "P"
+const idadeMinima = 2
+
+const adocao = idade >= idadeMinima || porte === "P"
+                ? "sim"
+                : porte === "P"
+                ? "sim"
+                : "não"
+
+console.log(adocao)
