a11f4d629384ad77d58d1f9d51d35509ed7e6012
IgorCruz <igorconf@hotmail.com>
2025-05-30 18:54:51 -0300

    exercices

---
+++ exercicios/08-exercicio.js
@@ -0,0 +1,7 @@
+const quantidadePestiscosDisponiveis = 4
+
+for (let petisco = 1; petisco <= quantidadePestiscosDisponiveis; petisco++) {
+    console.log(`Dando o petisco de n ${petisco} para o dog`)
+}
+
+console.log("Satisfeito!!")
